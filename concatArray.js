3110f72665359e3a06a5a00ea32c2b6d4386bcf8
Thanakorn <thanakorn.wong@kmutt.ac.th>
2022-09-07 14:33:39 +0700

    testGit2

--- concatArray.js
+++ concatArray.js
@@ -3,5 +3,6 @@ const { template } = require('@babel/core')
 function concatArray(array1, array2) {
   //code here
   console.log("testGit")
+  console.log("testGit")
 }
 module.exports = concatArray
