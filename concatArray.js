cf8f759783bb70654b81943e94a9993e021039aa
Thanakorn <thanakorn.wong@kmutt.ac.th>
2022-09-07 15:05:53 +0700

    Commit_1

--- concatArray.js
+++ concatArray.js
@@ -1,8 +1,19 @@
-const { template } = require('@babel/core')
+const { template } = require("@babel/core");
 
 function concatArray(array1, array2) {
+  let arr1 = new Array()
+  arr1 = array1
+  let arr2 = new Array()
+  arr2 = array2
   //code here
-  console.log("testGit")
-  console.log("testGit")
+  if (arr1 == [] && arr2 !== []) {
+    return arr2;
+  } else if (arr1 !== [] && arr2 == []) {
+    return arr1;
+  }else if(arr1 != [] && arr2 != []){
+    return arr1.concat(arr2)
+  }else{
+    return 'empty array'
+  }
 }
-module.exports = concatArray
+module.exports = concatArray;
